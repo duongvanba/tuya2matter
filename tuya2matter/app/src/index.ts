5234005551e1b428a77fc05bbb2968e163fb5dda
Ba <BA>
2025-08-20 00:11:00 +0700

    Fix crypto

--- tuya2matter/app/src/index.ts
+++ tuya2matter/app/src/index.ts
@@ -1,3 +1,5 @@
+process.env.MATTER_NODEJS_CRYPTO = '0'
+
 import { Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core"; 
 import { CloudSync } from "./services/CloudSync.js";
