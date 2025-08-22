7220d7bb55efbf97ce8aeb3a51ad3a6ff8987837
Ba <BA>
2025-08-22 15:44:10 +0700

    update outlet

--- tuya2matter/app/src/libs/tuya2matter/Tuya2Matter.ts
+++ tuya2matter/app/src/libs/tuya2matter/Tuya2Matter.ts
@@ -20,7 +20,7 @@ export class Tuya2Matter {
     ) { }
 
     #getMapper() {
-        if (['kg', 'tdq'].includes(this.tuya.category)) return new Tuya2MatterSwitch(this.aggregator, this.tuya)
+        if (['kg', 'tdq','cz'].includes(this.tuya.category)) return new Tuya2MatterSwitch(this.aggregator, this.tuya)
         if (this.tuya.category == 'cl') return new Tuya2MatterCover(this.aggregator, this.tuya)
         if (this.tuya.category == 'hps') return new Tuya2MatterOccupancySensor(this.aggregator, this.tuya)
         if (this.tuya.category == 'mcs') return new Tuya2MatterBinarySensor(this.aggregator, this.tuya)
